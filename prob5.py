bb570e345272e80625754dd6deb9737ee1151b14
sotirisnik <sotirisnik@gmail.com>
2014-05-24 15:55:35 +0300

    problem 5

--- prob5.py
+++ prob5.py
@@ -1,1 +1,1 @@
-def gcd( a, b ):    if b == 0:        return ( a )    return gcd( b, a%b )def lcm( a, b ):    return ( (a*b) / gcd(a,b) )    ans = 1for i in range( 2, 21):    ans = lcm( ans, i )    print "%d" % (ans)
+def gcd( a, b ):    if b == 0:        return ( a )    return gcd( b, a % b )def lcm( a, b ):    return ( (a*b) / gcd(a,b) )    ans = 1for i in range( 2, 21):    ans = lcm( ans, i )    print "%d" % (ans)
